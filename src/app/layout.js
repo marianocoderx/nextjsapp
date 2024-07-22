6fabf9a7a8752001fa08889d0cba551702b4c127
marianobugallo100 <marianobugallo100@gmail.com>
2024-07-22 20:37:26 +0000

    enabling links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,8 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main></main>
+        <main>
+          hola mundo
+        </main>
   );
 }
